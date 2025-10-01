f17e1d2229871724c8b8f26db75241ba55e4eddd
kevinc02@naver.com <kevinc02@naver.com>
2025-10-01 17:07:00 +0900

    전체 수정

--- src/pages/main-page/components/prologue/Prologue.jsx
+++ src/pages/main-page/components/prologue/Prologue.jsx
@@ -1,25 +1,22 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 
 // Z-0 레이어 (가장 아래)
-import Z0Layer from './Z0Layer';
+import Z0Layer from './Z0Layer'; 
 
 // Z-10 레이어 (Snap Scroll 기능)
-import Z10Layer from './Z10Layer';
+import Z10Layer from './Z10Layer'; 
 
 // Z-20 레이어 (이미지 배경)
-import Z20Layer from './Z20Layer';
+import Z20Layer from './Z20Layer'; 
 
 // Z-30 레이어 (이미지 오버레이)
-import Z30Layer from './Z30Layer';
+import Z30Layer from './Z30Layer'; 
 
 import HandLight from '../../effects/HandLight';
 
 const Prologue = () => {
   // SnapScroll에 연결할 ref 생성
   const snapScrollRef = useRef();
-
-  // 마우스 위치 상태
-  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
 
   // wheel 이벤트를 SnapScroll로 전달
   const handleWheel = (e) => {
@@ -31,20 +28,10 @@ const Prologue = () => {
     }
   };
 
-  // 마우스 위치 추적
-  const handleMouseMove = (e) => {
-    const rect = e.currentTarget.getBoundingClientRect();
-    setMousePos({
-      x: e.clientX - rect.left,
-      y: e.clientY - rect.top
-    });
-  };
-
   return (
-    <div
-      className="relative mx-auto overflow-hidden"
+    <div 
+      className="relative mx-auto overflow-hidden" 
       style={{ width: '1920px', height: '1080px' }}
-      onMouseMove={handleMouseMove}
     >
       <Z0Layer />
       {/* SnapScroll의 ref를 Z10Layer로 전달 */}
@@ -52,8 +39,7 @@ const Prologue = () => {
       <Z20Layer />
       {/* wheel 이벤트 핸들러를 Z30Layer로 전달 */}
       <Z30Layer onWheel={handleWheel} />
-      {/* HandLight에 마우스 위치 전달 */}
-      <HandLight mousePos={mousePos} />
+      {/* <HandLight /> */}
     </div>
   );
 };
--- src/pages/main-page/components/prologue/Z10Layer.jsx
+++ src/pages/main-page/components/prologue/Z10Layer.jsx
@@ -1,17 +1,59 @@
 import React from 'react';
+import SnapScroll from '../../effects/SnapScroll';
 
-const HandLight = ({ mousePos }) => {
+const Z10Layer = ({ snapScrollRef }) => {
+  const snapTexts = [
+    `어느 날, 단어를 재료 삼아\n현실을 만들어내는 ‘레시피’가 세상에 흩어졌다.`,
+    `그것은 곧 프롬프팅 엔지니어링,\n언어를 다루는 비밀의 조리법이었다.`,
+    `이제, 당신은 그 레시피를 배우고 익히기 위해 \n프롬프팅 레시피 아카이브에 들어왔다.`
+  ];
+
+  // 마스킹은 배경 div에만 적용
+  const maskStyle = {
+    WebkitMaskImage: `
+      linear-gradient(to bottom, black 0 100px, transparent 100px 180px, black 180px 100%),
+      linear-gradient(to right, black 0 calc(50% - 250px), transparent calc(50% - 250px) calc(50% + 250px), black calc(50% + 250px) 100%)
+    `,
+    maskImage: `
+      linear-gradient(to bottom, black 0 100px, transparent 100px 180px, black 180px 100%),
+      linear-gradient(to right, black 0 calc(50% - 250px), transparent calc(50% - 250px) calc(50% + 250px), black calc(50% + 250px) 100%)
+    `,
+    WebkitMaskComposite: 'intersect',
+    maskComposite: 'intersect'
+  };
+
   return (
-    <div
-      className="fixed inset-0 z-40 pointer-events-none"
-      style={{
-        maskImage: `radial-gradient(ellipse 256.5px 231px at ${mousePos.x}px ${mousePos.y}px, rgba(0,0,0,1) 0%, rgba(0,0,0,0.8) 40%, rgba(0,0,0,0) 100%)`,
-        WebkitMaskImage: `radial-gradient(ellipse 256.5px 231px at ${mousePos.x}px ${mousePos.y}px, rgba(0,0,0,1) 0%, rgba(0,0,0,0.8) 40%, rgba(0,0,0,0) 100%)`,
-      }}
-    >
-      <div className="w-full h-full bg-black" />
+    <div className="absolute w-screen h-screen top-0 left-0 z-10">
+      {/* 마스킹된 배경만 따로 */}
+      <div
+        className="absolute w-full h-full bg-[#f5f5f5]"
+        style={maskStyle}
+      />
+      {/* 투명 마스킹 위치에 빨간 테두리 */}
+      <div
+        className="
+          absolute
+          w-[500px] h-[80px]
+          top-[100px] left-1/2 -translate-x-1/2
+          rounded-lg
+          pointer-events-none
+          z-20
+        "
+      />
+      {/* SnapScroll은 마스킹 영향 밖에서 렌더링 */}
+      <div 
+        className="
+          absolute
+          flex items-center justify-center
+          w-[500px] h-[80px] top-[100px]
+          left-1/2 -translate-x-1/2
+          rounded-lg z-30
+        "
+      >
+        <SnapScroll ref={snapScrollRef} snaps={snapTexts} />
+      </div>
     </div>
   );
 };
 
-export default HandLight;
+export default Z10Layer;
